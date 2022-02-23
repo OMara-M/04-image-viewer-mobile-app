110dfe99cb92e0b8f63dfbf52dac54fd56167198
Matthew O'Mara <tuh39725@temple.edu>
2022-02-23 13:03:11 -0500

    image adapter

--- app/src/main/java/edu/temple/photoviewer/ImageAdapter.kt
+++ app/src/main/java/edu/temple/photoviewer/ImageAdapter.kt
@@ -1,4 +1,29 @@
 package edu.temple.photoviewer
 
-class ImageAdapter {
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import android.view.View
+import android.widget.ImageView
+
+class ImageAdapter(
+    val context: Context,
+    val images: List<Int>,
+    val names: List<String>,
+) : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(
+            R.layout.
+        )
+    }
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
 }
